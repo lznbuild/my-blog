3434276f19b92d5a5d3eef0bb40fb26797e91241
lzn <lznbuild@163.com>
2020-07-28 23:14:30 +0800

    柯里化的添加

--- test.js
+++ test.js
@@ -1,11 +1,10 @@
-const http = require('http');
-const url = require('url');
+const getData = () => new Promise(resolve => setTimeout(() => resolve("data"), 1000))
 
-http.createServer(function (req, res) {
-  let pathname = url.parse(req.url);
-
-  if (pathname != '/favicon.ico') {
-    console.log(pathname, 'pathname');
-  }
-}).listen(3000)
+async function test() {
+  const data = await getData()
+  console.log('data: ', data);
+  const data2 = await getData()
+  console.log('data2: ', data2);
+  return 'success'
+}
 
