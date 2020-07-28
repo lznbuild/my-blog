fe91ac9445bbdffccd396f6d1817f6c55319984a
wb-lzn587531 <wb-lzn587531@alibaba-inc.com>
2020-07-28 13:44:32 +0800

    'node基础'

--- test.js
+++ test.js
@@ -1,11 +1,24 @@
-const http = require('http');
-const url = require('url');
+const inquirer = require('inquirer')
 
-http.createServer(function (req, res) {
-  let pathname = url.parse(req.url);
+const promptList = [{
+    type: 'input',
+    message: '设置一个用户名:',
+    name: 'name',
+    default: "test_user" // 默认值
+},{
+    type: 'input',
+    message: '请输入手机号:',
+    name: 'phone',
+    validate: function(val) {
+        if(val.match(/\d{11}/g)) { // 校验位数
+            return val;
+        }
+        return "请输入11位数字";
+    }
+}];
 
-  if (pathname != '/favicon.ico') {
-    console.log(pathname, 'pathname');
-  }
-}).listen(3000)
 
+// inquirer.prompt(promptList).then(answers => {
+//   console.log(answers,'结果'); // 返回的结果
+// });
+console.log(process.env.NODE_ENV);
