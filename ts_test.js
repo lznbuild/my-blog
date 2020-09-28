1e10eadd7096f73c7679cf007e5b06de57933b88
wb-lzn587531 <wb-lzn587531@alibaba-inc.com>
2020-09-28 13:46:30 +0800

    'test'

--- ts_test.js
+++ ts_test.js
@@ -1,1 +1,1 @@
-var a = new Boolean(false);
+// enum Color {state1 = '未支付',state2="支付失败", state3="支付成功"}
--- ts_test.ts
+++ ts_test.ts
@@ -1,1 +1,12 @@
-let a:boolean = new Boolean(false);
+// enum Color {state1 = '未支付',state2="支付失败", state3="支付成功"}
+
+// let c:Color = Color.state1;
+
+// console.log(c,'c');
+
+// let a:number = (c as string).length
+
+// console.log(a);
+
+
+
